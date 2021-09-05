cfa2387770ca1f505488be739bed54a1d1072c8f
Amulyakanaparthi <86307368+Amulyakanaparthi@users.noreply.github.com>
2021-09-05 20:03:41 +0530

    first commit

---
+++ src/test/TestNG.java
@@ -0,0 +1,15 @@
+package test;
+
+
+	
+	import org.testng.annotations.Test;
+
+	public class TestNG {
+	  @Test
+	  public void testngexamplee() {
+		  
+		  amulya.sonardemo.main("demo");
+		  System.out.println("test is called");
+	  }
+	}
+
